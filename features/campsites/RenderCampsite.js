9fff968d90ddade0e6566258a3e4c143d6430ddf
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2024-12-16 16:53:45 -0500

    comments/icon campsite info

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,11 +1,11 @@
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View, StyleSheet } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = ({ campsite, isFavorite, markFavorite }) => {
 
     if(campsite){
         return (
-            <Card containerStyle={{ padding: 0 }}>
+            <Card containerStyle={styles.cardContainer}>
                 <Card.Image source={campsite.image}>
                     <View style={{ justifyContent: 'center', flex: 1 }}> 
                         <Text style={{color: 'white',textAlign: 'center',fontSize: 20}}>
@@ -16,10 +16,26 @@ const RenderCampsite = ({ campsite }) => {
                 <Text style={{ margin: 20 }}>
                     {campsite.description}
                 </Text>
+                <Icon
+                    name={isFavorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => isFavorite ? console.log("Already favorited") : markFavorite()}
+                />
             </Card>
         )
     }   
     return <View />;
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        padding: 0,
+        margin: 0,
+        marginBottom: 20
+    }
+});
 
 export default RenderCampsite;
