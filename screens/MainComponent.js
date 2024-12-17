4c33067886227b5b05dd79b24b70499ddeac3ffd
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2024-12-17 14:38:21 -0500

    custom side drawer

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,14 +1,14 @@
-import { Platform, View, StyleSheet } from 'react-native';
+import { Platform, View, StyleSheet, Image, Text } from 'react-native';
 import { Icon } from 'react-native-elements';
 import Constants from 'expo-constants';
 import DirectoryScreen from "./DirectoryScreen";
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
-
+import logo from '../assets/images/logo.png';
 
 const Drawer = createDrawerNavigator();
 
@@ -120,6 +120,20 @@ const DirectoryNavigator = () => {
     )
 }
 
+const CustomDrawerContent = (props) => (
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{ flex: 1 }}>
+                <Image source={logo} style={styles.drawerImage} />
+            </View>
+            <View style={{ flex: 2 }}>
+                <Text style={styles.drawerHeaderText}>NuCamp</Text>
+            </View> 
+        </View>
+        <DrawerItemList {...props} labelStyle={{ fontWeight: 'bold' }} />
+    </DrawerContentScrollView>
+)
+
 const Main = () => {
     return (
         <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
@@ -127,6 +141,7 @@ const Main = () => {
                 initialRouteName='HomeNav'
                 screenOptions={{ drawerStyle: { 
                 backgroundColor: '#CEC8FF' }}}
+                drawerContent={CustomDrawerContent}
             >
                 <Drawer.Screen
                     name='HomeNav'
@@ -202,6 +217,24 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         color: '#fff',
         fontSize: 24
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
     }
 });
 
